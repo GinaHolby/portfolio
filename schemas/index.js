050c6c2e193fcd8cc5f2f4d5c0d926ed6861d0ff
GinaHolby <91539886+GinaHolby@users.noreply.github.com>
2023-03-18 14:57:50 +0100

    pprtfolio

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,15 @@
-export const schemaTypes = []
+
+
+// schemas/index.js
+
+import ingredient from './ingredient'
+import instruction from './instruction'
+import recipie from './recipie'
+
+export const schemaTypes = [
+
+    recipie,
+    ingredient,
+    instruction
+
+]
